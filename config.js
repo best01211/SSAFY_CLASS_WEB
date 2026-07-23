9ba461e4836d6be357df51a1fd9b884d551f0a83
best01211 <jinwoobest.andy01@gmail.com>
2026-07-23 21:28:59 +0900

    Fix Supabase base URL

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG={
-    SUPABASE_URL:"https://ukxzlhpzgfwfbifygkjd.supabase.co/rest/v1/",
+    SUPABASE_URL:"https://ukxzlhpzgfwfbifygkjd.supabase.co",
     SUPABASE_ANON_KEY:"sb_publishable_tGdQ1TgdAeQb0RZfOr_NDg_cArrzoQv"
 };
