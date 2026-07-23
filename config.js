6db325a4da875b5d92238545dc0f3027f06062a7
best01211 <jinwoobest.andy01@gmail.com>
2026-07-23 21:22:24 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,4 @@
-window.APP_CONFIG={SUPABASE_URL:"https://YOUR_PROJECT_ID.supabase.co",SUPABASE_ANON_KEY:"YOUR_SUPABASE_ANON_KEY"};
+window.APP_CONFIG={
+    SUPABASE_URL:"https://YOUR_PROJECT_ID.supabase.co",
+    SUPABASE_ANON_KEY:"sb_publishable_tGdQ1TgdAeQb0RZfOr_NDg_cArrzoQv"
+};
