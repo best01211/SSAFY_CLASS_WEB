c50586b8df3c695f47de77d163bd796708e443cc
best01211 <jinwoobest.andy01@gmail.com>
2026-07-23 21:26:43 +0900

    Fix Supabase project URL

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG={
-    SUPABASE_URL:"https://YOUR_PROJECT_ID.supabase.co",
+    SUPABASE_URL:"https://ukxzlhpzgfwfbifygkjd.supabase.co/rest/v1/",
     SUPABASE_ANON_KEY:"sb_publishable_tGdQ1TgdAeQb0RZfOr_NDg_cArrzoQv"
 };
